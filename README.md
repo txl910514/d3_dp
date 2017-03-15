647d4394039057e9b6d6154e2c918084c77b443d
txl910514 <txl910514@sina.com>
2017-03-15 17:52:52 +0800

    d3属性

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # d3_dp
 d3_dp
+
+# v4文档
+* https://github.com/tianxuzhang/d3.v4-API-Translation
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 /**
  * Created by txl on 17-2-13.
  */
+console.log(1,d3.select('body').attr('id', 'mm'));
+console.log(1,d3.select('body').empty());
+console.log(2,d3.select('#mm').node());
+console.log(3,d3.select('.mm').size());
+console.log(4,d3.select('svg'));
+console.log(5,d3.selectAll('svg'));
+
+var dom = document.getElementsByTagName('svg');
+console.log(6,d3.select(dom));
