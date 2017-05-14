fe371e518d3fb3b3b511ad17694d1a5141a0913a
txl910514 <txl910514@sina.com>
2017-05-14 23:20:45 +0800

    柱状图折线图

--- src/js/line_bar.js
+++ src/js/line_bar.js
@@ -1,37 +1,44 @@
 /**
- * Created by txl on 17-5-8.
+ * Created by txl on 17-5-13.
  */
-
 var data = [
   {
     name:'呼吸机1',
-    value:90
+    line:90,
+    bar: 83
   },
   {
     name:'呼吸机2',
-    value:40
+    line:40,
+    bar: 32
   },
   {
     name:'呼吸机3',
-    value:70
+    line:70,
+    bar:64
   },
   {
     name:'呼吸机h',
-    value:60
+    line:60,
+    bar: 53
   },
   {
     name:'呼吸机4',
-    value:80
+    line:80,
+    bar: 72
   },
   {
     name:'呼吸机5',
-    value:20
+    line:20,
+    bar: 16
   },
   {
     name:'呼吸机6',
-    value:50
+    line:50,
+    bar: 43
   }
 ]
+
 var svg = d3.select(".content")
   .append('svg')
   .attr('width', '100%')
@@ -53,79 +60,83 @@ var stop2 = linearGradient.append("stop")
   .attr("offset","100%")
   .style("stop-color", '#2ede7d');
 
-var areaGradient = defs.append("linearGradient")
-  .attr("id","areaColor")
+var barGradient = defs.append("linearGradient")
+  .attr("id","barColor")
   .attr("x1","0%")
   .attr("y1","0%")
   .attr("x2","0%")
   .attr("y2","100%");
 
-areaGradient.append("stop")
+barGradient.append("stop")
   .attr("offset","0%")
-  .style("stop-color", '#2ede7d');
+  .style("stop-color", '#2ea6de');
 
-areaGradient.append("stop")
+barGradient.append("stop")
   .attr("offset","100%")
-  .style("stop-color", '#1f2a33');
+  .style("stop-color", '#2ede7d');
 
 var svgWidth = document.querySelector('.content').offsetWidth,
   svgHeight = document.querySelector('.content').offsetHeight;
 
 var margin = {left:20, top:10, bottom:20, right:10},
-width = svgWidth - margin.left - margin.right,
+  width = svgWidth - margin.left - margin.right,
   height = svgHeight - margin.top - margin.bottom;
 
 var svgG = svg.append('g')
   .attr('transform', "translate("+ margin.left+ ","+ margin.top +")");
 
+var lineMax = d3.max(data, function (d) {
+  return d.line;
+});
+var barMax = d3.max(data, function (d) {
+  return d.bar;
+});
+
 var y = d3.scaleLinear().range([height, 0])
-  .domain([0, d3.max(data, function (d) {
-    return d.value;
-  })]);
+  .domain([0, Math.max(lineMax, barMax)]);
 
 var x = d3.scaleBand().rangeRound([0, width])
-  .paddingInner(1)
-  .paddingOuter(0.5)
+  .padding(0.85)
   .domain(data.map(function(d) { return d.name; }));
 
 var line = d3.line()
   .x(function(d) {
-    console.log(d);
     return  x(d.name);
   })
   .y(function(d) {
-    console.log(y(d.value))
-    return  y(d.value);
+    return  y(d.line);
   })
-  .curve(d3.curveCatmullRom.alpha(0.9));
-
-var area = d3.area()
-  .x(function(d) {
-    return x(d.name);
-  })
-  .y0(y(0))
-  .y1(function(d) {
-    return y(d.value);
-  })
-  .curve(d3.curveCatmullRom.alpha(0.9));
+  .curve(d3.curveCatmullRom.alpha(0.5));
 
 var symbol = d3.symbol()
-    .size(64)
-    .type(d3.symbolCircle);
+  .size(64)
+  .type(d3.symbolCircle);
 
 var color = d3.scaleSequential(d3.interpolate("#2ea6de","#2ede7d"));
 
 svgG.append('g')
-  .attr('class', 'area')
-  .append("path")
-  .datum(data)
-  .attr("class", "line_path")
-  .attr('opacity',0.4)
-  .attr("d", area)
-  .style('fill','url(#'+ areaGradient.attr("id") +')');
+  .attr('class', 'bagG')
+  .selectAll('.bar')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('x', function(d) {
+    console.log(x.bandwidth());
+    return x(d.name)
+  })
+  .attr('y', function(d) {
+    return y(d.bar)
+  })
+  .attr('width', x.bandwidth())
+  .attr('height', function(d) {
+    return height - y(d.bar);
+  })
+  .attr('fill', 'url(#'+ barGradient.attr("id") +')')
+
 
 svgG.append('g')
   .attr('class', 'line')
+  .attr('transform', 'translate('+ x.bandwidth() / 2 +', 0)')
   .append("path")
   .datum(data)
   .attr("class", "line_path")
@@ -140,13 +151,14 @@ svgG.select('.line')
   .append('path')
   .attr('d', symbol.type(d3.symbolCircle))
   .attr('transform', function(d) {
-    return 'translate('+ x(d.name) + ',' + y(d.value) + ')'
+    return 'translate('+ x(d.name) + ',' + y(d.line) + ')'
   })
   .attr("stroke-width", 3)
   .attr('fill', '#fff')
   .style('stroke', function(d){
     return color(x(d.name)/width)
   });
+
 
 
 
@@ -159,9 +171,9 @@ axis_x.selectAll("text").style("fill","#899baa");
 axis_x.selectAll("line").style("display","none");
 axis_x.selectAll("path").style("display","none");
 /*axis_x.selectAll('.tick')
-  .attr('transform', function (d) {
-    return "translate("+ x(d) +",0)";
-  });*/
+ .attr('transform', function (d) {
+ return "translate("+ x(d) +",0)";
+ });*/
 
 var lineMargin = 15;
 var axis_y = svgG.append("g")
@@ -175,6 +187,3 @@ axis_y.selectAll("line")
   .attr('opacity','0.2')
   .attr('transform', 'translate('+ lineMargin +',0)');
 axis_y.selectAll("path").style("display","none");
-
-
-
